e7eb6530265aeb9f092eb17126fc7222f3aa8399
bneely04 <bneely04@gmail.com>
2025-12-06 15:28:01 -0500

    Running grovers twice for better results

--- grovers.py
+++ grovers.py
@@ -7,32 +7,32 @@ qc = QuantumCircuit(3)
 
 # Initial superposition
 qc.h([0, 1, 2])
-
+for i in range(2): 
 #Phase Flip |101>
-qc.x(1)            
-qc.ccz(0, 1, 2)    
-qc.x(1)            
+    qc.x(1)            
+    qc.ccz(0, 1, 2)    
+    qc.x(1)            
 
-# Diffuser
-qc.h([0, 1, 2])
-qc.x([0, 1, 2])
-#Decomposed CCX
-qc.cx(1, 2)
-qc.tdg(2)
-qc.cx(0, 2)
-qc.t(2)
-qc.cx(1, 2)
-qc.t(1)
-qc.tdg(2)
-qc.cx(0, 2)
-qc.cx(0, 1)
-qc.t(0)
-qc.tdg(1)
-qc.cx(0, 1)
-qc.t(2)
-#Diffuser
-qc.x([0, 1, 2])
-qc.h([0, 1, 2])
+    # Diffuser
+    qc.h([0, 1, 2])
+    qc.x([0, 1, 2])
+    #Decomposed CCX
+    qc.cx(1, 2)
+    qc.tdg(2)
+    qc.cx(0, 2)
+    qc.t(2)
+    qc.cx(1, 2)
+    qc.t(1)
+    qc.tdg(2)
+    qc.cx(0, 2)
+    qc.cx(0, 1)
+    qc.t(0)
+    qc.tdg(1)
+    qc.cx(0, 1)
+    qc.t(2)
+    #Diffuser
+    qc.x([0, 1, 2])
+    qc.h([0, 1, 2])
 
 
 
